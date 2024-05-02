5f8af22090fdd6bc86d9c32e4302e2b1b0093645
Jtangyi <20320884@mylife.unisa.ac.za>
2024-05-02 13:37:30 -0700

    Your commit message here

---
+++ App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './components/screens/Home';
+import MyCart from './components/screens/MyCart';
+import ProductInfo from './components/screens/ProductInfo';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
+import { EditProfile } from './components/screens';
+import BottomTabNav from './navigations/BottomTabNav';
+
+
+
+
+export default function App(){
+
+SplashScreen.preventAutoHideAsync();
+  const Stack = createNativeStackNavigator();
+
+  const [fontsLoaded] = useFonts({
+    black: require('./assets/fonts/Inter-Black.ttf'),
+    bold: require('./assets/fonts/Inter-Bold.ttf'),
+    medium: require('./assets/fonts/Inter-Medium.ttf'),
+    regular: require('./assets/fonts/Inter-Regular.ttf'),
+    semiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async ()=>{
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null
+  }
+
+  return (
+    <NavigationContainer onReady={onLayoutRootView}>
+      <Stack.Navigator
+        initialRouteName='BottomTabNavigation'>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+         <Stack.Screen
+          name="BottomTabNavigation"
+          component={BottomTabNav}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="EditProfile"
+          component={EditProfile}
+          options={{
+            headerShown: false
+          }}/>
+    <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="MyCart" component={MyCart} />
+        <Stack.Screen name="ProductInfo" component={ProductInfo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
